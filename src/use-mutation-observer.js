2b692ef3cc3e22a4dbcce518685cab5f9634427a
labchuk <labchuks70@gmail.com>
2023-04-04 16:27:20 +0300

    hooks

---
+++ src/use-mutation-observer.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import { useLatest } from './use-latest';
+import { useCombinedRef } from './use-combined-ref';
+
+export function useMutationObserver(cb, options) {
+    const [element, setElement] = useState(null);
+    const latestCb = useLatest(cb);
+
+    useEffect(() => {
+        if (!element) return;
+
+        const observer = new MutationObserver((...args) => {
+            latestCb.current(...args);
+        });
+
+        observer.observe(element, options);
+
+        return () => observer.disconnect();
+    }, [element, latestCb, options]);
+
+    return setElement;
+}
+
+export function MutationObserverComponent({
+                                              children,
+                                              onMutation,
+                                              options,
+                                              nodeRef,
+                                          }) {
+    const mutationRef = useMutationObserver(onMutation, options);
+
+    const combinedRef = useCombinedRef(nodeRef, mutationRef);
+
+    return children(combinedRef);
+}
---
+++ src/use-throttle-debounce.js
@@ -0,0 +1,24 @@
+import { useEffect, useMemo } from 'react';
+import { useLatest } from './use-latest';
+import {debounce,throttle} from 'lodash'
+
+function makeDebouncedHook(debounceFn) {
+    return function useDebounce(cb, ms) {
+        const latestCb = useLatest(cb);
+
+        const debouncedFn = useMemo(
+            () =>
+                debounceFn((...args) => {
+                    latestCb.current(...args);
+                }, ms),
+            [ms, latestCb]
+        );
+
+        useEffect(() => () => debouncedFn.cancel(), [debouncedFn]);
+
+        return debouncedFn;
+    };
+}
+
+export const useDebounce = makeDebouncedHook(debounce);
+export const useThrottle = makeDebouncedHook(throttle);
